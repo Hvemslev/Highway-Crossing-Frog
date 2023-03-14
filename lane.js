0e7645f636e0318d76afb2e5bc962843f862a7a5
mark6959 <mark6959@hotmail.com>
2023-03-14 09:11:00 +0100

    haha

--- lane.js
+++
@@ -1,5 +0,0 @@
-class lane{
-    constructor(){
-
-    }
-}
--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
-function draw() {
-  background(220);
-  rect(20,20,20,20)
-}
+//function draw() {
+  //background(220);
+  //rect(20,20,20,20)
+//}
